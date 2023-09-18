b1bbe185ed6cd5ba6e57f1d1942f3292ebb04779
bheston1 <bheston721@gmail.com>
2023-09-18 04:25:41 -0400

    add menu controller

---
+++ TriviaGame/MenuController.cs
@@ -0,0 +1,14 @@
+﻿namespace TriviaGame
+{
+    internal class MenuController
+    {
+        internal static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Select an option:");
+            Console.WriteLine(@"1 - Play game
+2 - View previous games
+3 - Close application");
+        }
+    }
+}
