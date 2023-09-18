d40303e728373a6ca1a49b391d40a897d9d4ae72
bheston1 <bheston721@gmail.com>
2023-09-18 04:25:53 -0400

    add helper functions

---
+++ TriviaGame/HelperFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGame
+{
+    internal class HelperFunctions
+    {
+        internal static void TitleScreen()
+        {
+            Console.WriteLine("                     ▄▄              ▄▄                                                          \r\n███▀▀██▀▀███         ██              ██               ▄▄█▀▀▀█▄█                                  \r\n█▀   ██   ▀█                                        ▄██▀     ▀█                                  \r\n     ██    ▀███▄███▀███ ▀██▀   ▀██▀▀███  ▄█▀██▄     ██▀       ▀ ▄█▀██▄ ▀████████▄█████▄   ▄▄█▀██ \r\n     ██      ██▀ ▀▀  ██   ██   ▄█    ██ ██   ██     ██         ██   ██   ██    ██    ██  ▄█▀   ██\r\n     ██      ██      ██    ██ ▄█     ██  ▄█████     ██▄    ▀████▄█████   ██    ██    ██  ██▀▀▀▀▀▀\r\n     ██      ██      ██     ███      ██ ██   ██     ▀██▄     ████   ██   ██    ██    ██  ██▄    ▄\r\n   ▄████▄  ▄████▄  ▄████▄    █     ▄████▄████▀██▄     ▀▀███████▀████▀██▄████  ████  ████▄ ▀█████▀\r\n                                                                                                 \r\n                                                                                                 \r\n");
+
+
+            Console.WriteLine("Press ENTER");
+            PressEnter();
+            MenuController.ShowMenu();
+        }
+
+        private static void PressEnter()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+            } while (key.Key != ConsoleKey.Enter);
+        }
+    }
+}
