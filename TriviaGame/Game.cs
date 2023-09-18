8c736e6dcb8f5aa255b284ea192f068f8cf395b2
bheston1 <bheston721@gmail.com>
2023-09-18 04:34:07 -0400

    add game class

---
+++ TriviaGame/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGame
+{
+    internal class Game
+    {
+        internal static void Start()
+        {
+            Console.WriteLine("                     ▄▄              ▄▄                                                          \r\n███▀▀██▀▀███         ██              ██               ▄▄█▀▀▀█▄█                                  \r\n█▀   ██   ▀█                                        ▄██▀     ▀█                                  \r\n     ██    ▀███▄███▀███ ▀██▀   ▀██▀▀███  ▄█▀██▄     ██▀       ▀ ▄█▀██▄ ▀████████▄█████▄   ▄▄█▀██ \r\n     ██      ██▀ ▀▀  ██   ██   ▄█    ██ ██   ██     ██         ██   ██   ██    ██    ██  ▄█▀   ██\r\n     ██      ██      ██    ██ ▄█     ██  ▄█████     ██▄    ▀████▄█████   ██    ██    ██  ██▀▀▀▀▀▀\r\n     ██      ██      ██     ███      ██ ██   ██     ▀██▄     ████   ██   ██    ██    ██  ██▄    ▄\r\n   ▄████▄  ▄████▄  ▄████▄    █     ▄████▄████▀██▄     ▀▀███████▀████▀██▄████  ████  ████▄ ▀█████▀\r\n                                                                                                 \r\n                                                                                                 \r\n");
+
+
+            Console.WriteLine("Press ENTER");
+            HelperFunctions.PressEnter();
+            MenuController.ShowMenu();
+        }
+    }
+}
