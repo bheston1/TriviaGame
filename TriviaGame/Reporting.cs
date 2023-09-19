055163e6f4f7aa3ae3792faf8a568d445e4570fb
bheston1 <bheston721@gmail.com>
2023-09-18 21:11:10 -0400

    add placeholder reporting class

---
+++ TriviaGame/Reporting.cs
@@ -0,0 +1,13 @@
+﻿namespace TriviaGame
+{
+    internal class Reporting
+    {
+        internal static void ShowReports()
+        {
+            Console.Clear();
+            Console.WriteLine("Reporting feature not yet implemented\n\nPress ENTER to return to menu");
+            HelperFunctions.PressEnter();
+            MenuController.MainMenu();
+        }
+    }
+}
