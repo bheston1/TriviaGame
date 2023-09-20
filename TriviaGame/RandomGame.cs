1099634fff0ad3786bdb27492473077c0311fbb2
bheston1 <bheston721@gmail.com>
2023-09-20 00:38:12 -0400

    add random game option

---
+++ TriviaGame/RandomGame.cs
@@ -0,0 +1,26 @@
+﻿namespace TriviaGame
+{
+    internal class RandomGame
+    {
+        internal static void GetCategory()
+        {
+            var random = new Random();
+            int gameRoll = random.Next(1, 4);
+
+            switch (gameRoll)
+            {
+                case 1:
+                    Animals.GetAnimalQuestions();
+                    break;
+
+                case 2:
+                    Geography.GetGeographyQuestions();
+                    break;
+
+                case 3:
+                    Science.GetScienceQuestions();
+                    break;
+            }
+        }
+    }
+}
